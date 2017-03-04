7f964bb8daab54c49a6110566e1937496c5922e7
Christopher Sharman <christopher.p.sharman@gmail.com>
2017-03-04 17:33:22 +0000

    Removed unused argument.

--- public/index.php
+++ public/index.php
@@ -7,7 +7,7 @@ $seed = isset($_POST['seed']) ? $_POST['seed'] : 1;
 $difficulty = isset($_POST['difficulty']) ? $_POST['difficulty'] : 3;
 
 // Generate a new puzzle.
-$puzzle = new \Kriptonic\Sudoku\Sudoku($seed, $difficulty, 100);
+$puzzle = new \Kriptonic\Sudoku\Sudoku($seed, $difficulty);
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Check the player's answers if they have submit the form.
